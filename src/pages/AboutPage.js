8db69f10976be6e3d6236842e6aa69d64777a184
gletshcer <wshiizhilh123@vip.qq.com>
2023-06-05 21:21:34 +0800

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about"></PageHero>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi est
+            molestiae debitis, necessitatibus culpa quasi! Voluptates facilis
+            porro consectetur eos ut architecto corrupti, earum ea rem animi
+            ipsum quaerat eum cupiditate est ex magnam sed illum distinctio
+            mollitia sit tempora maxime sequi nostrum amet. Cupiditate saepe
+            aliquam officiis aut commodi?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import SingleProduct from "./SingleProductPage";
+import Products from "./ProductsPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+  AuthWrapper,
+};
